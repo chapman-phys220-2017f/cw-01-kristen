16012f18cbb1f1616b8c29b7601029e25966abff
Justin Dressel <prof.justin.dressel@gmail.com>
2017-10-09 22:16:39 -0700

    Instructor comments

--- cw_01.py
+++ cw_01.py
@@ -8,3 +8,12 @@ cd testfiles
 touch file{001..100}.tmp
 cd/
 exit 0
+
+###
+# INSTRUCTOR COMMENTS:
+# The file name should end in .sh not .py for a Bash SHell file.
+# The second to last line should be "cd -", which changes back to the last
+# working directory.  Alternatively, you could define a variable at the
+# beginning "CWD=$(pwd)" that contains the current working directory, then
+# at the end change back to it: "cd $CWD"
+###
